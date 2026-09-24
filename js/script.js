35a2a2d39791f59ad819d0690ece1b90718e80e4
Fabi01a <gonzalezfabs@gmail.com>
2026-09-23 17:28:41 -0700

    color drop down currently disabled when design drop down hasn't been chosen

--- js/script.js
+++ js/script.js
@@ -16,12 +16,36 @@ otherJobRole.style.display = 'none';
 title.addEventListener('change', e => {
 //access the event
 const targetValue = e.target.value
-//
+//if 'other' is chosen for the event
 if (targetValue === 'other') {
+    //display the other field
     otherJobRole.style.display = 'inline-block';
 } else {
+    //otherwise keep it hidden
     otherJobRole.style.display = 'none';
-}    
+    }
+});    
 
+//T-Shirt Info Section
+//referencing Design element
+const design = document.getElementById('design');
+//referencing Color element
+const color = document.getElementById('color');
+//referencing the children property of Color
+const colorOptions = color.children
+//disable its property to true
+color.disabled = true;
+//listen for change event on the element
+design.addEventListener('change', e => {
+    //enable its property to false
+    color.disabled = false;
+    //loop over Color option elements
+    for (let i = 0; i < colorOptions.length;i++) {
+        //accessing the event
+        const eventValue = e.target.value;
+        //storing the current color's data type
+        const colorData = colorOptions[i].getAttribute('data-theme');
+        console.log(eventValue)
+        console.log(colorData)
+    }
 })
-
